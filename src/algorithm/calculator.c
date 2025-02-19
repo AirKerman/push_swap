8b8c0c9b0a333b3c26d5ae030103d947cb5c72e9
Rayan Kerman <rkerman@c3r3p6.42perpignan.fr>
2025-02-19 14:36:08 +0100

    addon (is min, position)

--- src/algorithm/calculator.c
+++ src/algorithm/calculator.c
@@ -6,21 +6,44 @@
 /*   By: rkerman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:25:08 by rkerman           #+#    #+#             */
-/*   Updated: 2025/02/18 18:51:11 by rkerman          ###   ########.fr       */
+/*   Updated: 2025/02/19 14:35:24 by rkerman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
-void	ft_calcul_lowcost(t_stack **stack_a, t_stack **stack_b, int *t)
+int	is_min(t_stack *stack_a, t_stack *stack_b)
 {
+	while (stack_b)
+	{
+		if (stack_a->value > stack_b->value)
+			return (0);
+		stack_b = stack_b->next;
+	}
+	return (1);
+}
 
+int	position(t_stack *)
+
+void	ft_calcul_lowcost(t_stack *stack_a, t_stack *stack_b, int *t)
+{
+	int		current;
+	int		shotcount;
+
+	while (stack_a)
+	{
+		if (is_min)
+		{
+			
+		}
+	}
 }
 
 void	ft_calcul_and_execute(t_stack **stack_a, t_stack **stack_b)
 {
 	int	target;
 
-	ft_calcul_lowcost(stack_a, stack_b, &target);
-	ft_execute();
+	ft_calcul_lowcost(*stack_a, *stack_b, &target);
+	//ft_execute();
 }
--- src/includes/push_swap.h
+++ src/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rkerman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 16:02:35 by rkerman           #+#    #+#             */
-/*   Updated: 2025/02/12 12:25:58 by rkerman          ###   ########.fr       */
+/*   Updated: 2025/02/19 14:14:33 by rkerman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ typedef struct t_list
 void	push_swap(t_stack **stack_a, t_stack **stack_b);
 void	ft_sortmin(t_stack **stack, int len);
 void	ft_sorter(t_stack **stack_a, t_stack **stack_b);
-
+void	ft_calcul_and_execute(t_stack **stack_a, t_stack **stack_b);
 /*
 
 	Utils
