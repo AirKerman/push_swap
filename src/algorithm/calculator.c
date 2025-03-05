6dc153cb7f91841fd2c03d9cc9ea51e48b0d50da
Rayan Kerman <rkerman@c3r3p6.42perpignan.fr>
2025-03-05 16:10:16 +0100

    first opti beta

--- src/algorithm/calculator.c
+++ src/algorithm/calculator.c
@@ -6,7 +6,7 @@
 /*   By: rkerman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:25:08 by rkerman           #+#    #+#             */
-/*   Updated: 2025/03/05 15:32:51 by rkerman          ###   ########.fr       */
+/*   Updated: 2025/03/05 16:09:35 by rkerman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,7 +120,16 @@ int	bellow_med(int len_a, int len_b, int pos_target, int pos_bullet)
 	int	shot;
 
 	shot = 0;
-	if ()
+	if ((pos_bullet < len_a / 2 && pos_target < len_b / 2)
+		|| (pos_bullet == len_a / 2 && len_a % 2 > 0
+		&& pos_target == len_b / 2 && len_b % 2 > 0)
+		|| (pos_bullet < len_a / 2 && pos_target >= len_b / 2
+		&& (pos_target - pos_bullet ) < (len_b - pos_target))
+		|| (pos_target < len_b / 2 && pos_bullet >= len_a / 2
+		&& ((pos_bullet - pos_target) < (len_a - pos_bullet))))
+		{
+
+		}
 }
 int	top_med(int len_a, int len_b, int pos_target, int pos_bullet)
 {
