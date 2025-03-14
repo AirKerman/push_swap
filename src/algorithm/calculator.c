f3cae4d672fc7cdf384e4fd5c1b037bca0487127
Rayan Kerman <rkerman@c3r3p6.42perpignan.fr>
2025-03-14 13:19:20 +0100

    optimisation for 500 number

--- src/algorithm/calculator.c
+++ src/algorithm/calculator.c
@@ -6,7 +6,7 @@
 /*   By: rkerman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:25:08 by rkerman           #+#    #+#             */
-/*   Updated: 2025/03/12 16:22:25 by rkerman          ###   ########.fr       */
+/*   Updated: 2025/03/14 13:13:24 by rkerman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,35 +86,6 @@ int	get_pos(t_stack *stack, int data)
 	return (0);
 }
 
-/*
-
-	si les deux son proche du centre suivre 1 direction peux raccourcir le chemin et faire econimisez des coups par la suite
-
-	par exemple 
-	
-	stack_a :
-	1 2 3 4 5 6 7 8 9
-
-	stack_b :
-	10 11 12 13 14 15 16 17 18
-
-	en voulant par exemple deplacer le 13 et le 6 il serais + judicieux de faire un coup combiner pour deplacer le 6 en 1 ere place
-	avec le 13 il restera - de coup a faire et cela divisera le nombre de coup effectuer donc apres le deplacement de 6 mmh enfin de
-	compte deplacer le 13 combiner du 6 avec un rr serais plus rentable (economie de 1 constant)
-
-	il serais + judicieux de commencer par le combiner en rr et pas en rrr le rrr est seulement si le rr n est pas rentable car le rr
-	est toujours plus avantageux
-
-	plus qu a trouver comment savoir si cest rentable de rr a la place de rrr pour ca il faudra stocker le nombre de coup en rrr et en rr
-	des deux valeur afin dy concocter une formule qui aura la solution il faudrais donc regarder si le fais de deplacer les 2 en meme temps
-	reduirais drastiquement ca position de la premiere place donc occupons nous de la fonction des rr
-
-	rrshot = (len - pos) = coup en reverse rotate
-
-	desormais il faut voir ou ce situation le chiffre concerner par un reverse rotate apres lexecution du rotate classic donc 
-	shotrotate classic - positionceluienreverserotate
-*/
-
 int	cmp_shot(int shot_a, int shot_b)
 {
 	if (shot_b > shot_a)
@@ -127,9 +98,11 @@ int	bellow_med(int len_a, int len_b, int pos_target, int pos_bullet)
 {
 	if ((pos_bullet <= len_a / 2 && pos_target <= len_b / 2)
 		|| (pos_bullet <= len_a / 2 && pos_target > len_b / 2
-		&& cmp_shot(pos_bullet, pos_target) <= cmp_shot(len_a - pos_bullet, len_b - pos_target))
+		&& cmp_shot(pos_bullet, pos_target) <= cmp_shot(len_a - pos_bullet, len_b - pos_target)
+		&& cmp_shot(pos_bullet, pos_target) <= pos_bullet + (len_b - pos_target))
 		|| (pos_target <= len_b / 2 && pos_bullet > len_a / 2
-		&& cmp_shot(pos_bullet, pos_target) <= cmp_shot(len_a - pos_bullet, len_b - pos_target))		
+		&& cmp_shot(pos_bullet, pos_target) <= cmp_shot(len_a - pos_bullet, len_b - pos_target)
+		&& cmp_shot(pos_bullet, pos_target) <= pos_target + (len_a - pos_bullet))		
 		)
 		return (cmp_shot(pos_bullet, pos_target) + 1);
 	return (0);
@@ -138,10 +111,12 @@ int	bellow_med(int len_a, int len_b, int pos_target, int pos_bullet)
 int	top_med(int len_a, int len_b, int pos_target, int pos_bullet)
 {
 	if ((pos_bullet > len_a / 2 && pos_target > len_b / 2)
-		|| (pos_bullet >= len_a / 2 && pos_target < len_b / 2 
-		&& cmp_shot(pos_bullet, pos_target) > cmp_shot(len_a - pos_bullet, len_b - pos_target))
-		|| (pos_target <= len_b / 2 && pos_bullet > len_a / 2 
-		&& cmp_shot(pos_bullet, pos_target) > cmp_shot(len_a - pos_bullet, len_b - pos_target))
+		|| (pos_bullet > len_a / 2 && pos_target <= len_b / 2 
+		&& cmp_shot(pos_bullet, pos_target) > cmp_shot(len_a - pos_bullet, len_b - pos_target)
+		&& cmp_shot(len_a - pos_bullet, len_b - pos_target) < pos_target + (len_a - pos_bullet))
+		|| (pos_target > len_b / 2 && pos_bullet <= len_a / 2 
+		&& cmp_shot(pos_bullet, pos_target) > cmp_shot(len_a - pos_bullet, len_b - pos_target)
+		&& cmp_shot(len_a - pos_bullet, len_b - pos_target) < pos_bullet + (len_b - pos_target))
 		)
 		return (cmp_shot(len_a - pos_bullet, len_b - pos_target) + 1);
 	return (0);
